3acb3ef3fb997d33e1c9672d91c3848da0d83e31
jhyun <jhyun@stunitas.com>
2021-04-19 18:32:20 +0900

    JPA & CRUD API 업데이트

---
+++ src/main/java/com/springboot/web/PostsApiController.java
@@ -0,0 +1,25 @@
+package com.springboot.web;
+
+import com.springboot.service.PostsService;
+import com.springboot.web.dto.PostsResponseDto;
+import com.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id) {
+        return  postsService.findById(id);
+    }
+}
